6d62a6f1fe7ec9a8d9a362da0ed17de85b5502d9
Dionysis Grigoropoulos <dgrig@erethon.com>
2023-02-02 21:57:06 +0200

    Quick dirty hack to push things

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 use clap::Parser;
 use reticulum_packet_rs::parse_packet;
 use std::io::{self, BufRead, BufReader};
+use hex;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[arg(long, short)]
     verbose: bool,
+
+    #[arg(long, short)]
+    tcp: bool,
 }
 
 fn main() -> io::Result<()> {
     let cli = Cli::parse();
     let stdin = io::stdin();
-    for packet in BufReader::new(stdin).split(0x7e) {
-        let packet = packet.unwrap();
-        if packet.len() < 36 {
-            continue;
+    if cli.tcp {
+        for packet in BufReader::new(stdin).lines() {
+            let raw_packet = packet.unwrap();
+            if raw_packet.len() > 35 {
+                let raw = hex::decode(raw_packet).unwrap_or("00".into());
+                if raw.len() > 35 {
+                    let a = parse_packet(raw);
+                    a.unwrap().debug_packet(cli.verbose);
+                }
+            }
         }
-        let a = parse_packet(packet);
-        a.unwrap().debug_packet(cli.verbose);
     }
     Ok(())
 }
